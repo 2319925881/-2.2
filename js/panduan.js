7f825337ed4d34a781f4b8a03dd1153761e1ef4e
2319925881 <2319925881@qq.com>
2018-12-18 19:02:10 +0800

    Initial commit

---
+++ js/panduan.js
@@ -0,0 +1,43 @@
+﻿function $(id) {return document.getElementById(id);}
+function checkReg( )
+{
+	var name=$("name").value;
+	var email=$("email").value;
+	var pwd=$("password").value;
+	var conpwd=$("confirmpasdsword").value;
+    var regex = /\@/g;//是否存在"@";
+	if(name==""||email==""||pwd==""||conpwd=="")
+	{
+		alert("请确认信息是否输入！！");
+	}
+	else{
+		if(name.length<6)
+		{
+			alert("用户名长度太短，至少6个字符！！");
+			$("name").value="";
+		}
+		else if(regex.test(email)==0)
+		{
+			alert("邮箱地址不正确！！");
+			$("email").value="";
+		}
+		else if(pwd.length<6){
+			alert("密码长度太短，至少6个字符！！");
+			$("password").value="";
+		}
+		else if(conpwd!=pwd)
+		{
+			alert("请重复输入密码时保持一致！！");
+			$("confirmpasdsword").value="";
+		}
+                else {
+                       alert("注册成功！！");
+                       $("name").value="";
+                       $("email").value="";
+                       $("password").value="";
+                       $("confirmpasdsword").value="";
+                    }
+	}
+}
+
+
---
+++ js/panduan2.js
@@ -0,0 +1,26 @@
+﻿function $(id) {return document.getElementById(id);}
+function checkReg2( )
+{
+	var name=$("name").value;
+	var pwd=$("password").value;
+	if(name==""||pwd=="")
+	{
+		alert("请确认信息是否输入！！");
+	}
+	else{
+		if(name.length<6)
+		{
+			alert("用户名长度太短，至少6个字符！！");
+			$("name").value="";
+		}
+		else if(pwd.length<6){
+			alert("密码长度太短，至少6个字符！！");
+			$("password").value="";
+		}
+                else {
+                       alert("登录成功！！");
+                       $("name").value="";
+                       $("password").value="";
+                    }
+	}
+}
